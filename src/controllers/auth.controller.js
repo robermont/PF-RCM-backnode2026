769f384fd1c7b83bf977f290d94c4b46dd48bc1b
rober <robermon@hotmail.com>
2026-07-08 19:10:05 -0300

    prueba login  tp

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,17 @@
 import { generateToken } from "../data/token.js";
 
+/*
+const default_user = {       
+    id: 1,       
+    email: "user@email.com",       
+    password: "strongPass123"     
+} 
+*/
 export async function login(req, res) {
     const { email, password } = req.body;
+    console.log("EMAIL: ",email)
+    console.log("EMAIL: ",password)
+
     if (email === "admin@gmail.com" && password === "123456") {
         const token = generateToken({ id: 1, email: email });
         res.json({ token });
